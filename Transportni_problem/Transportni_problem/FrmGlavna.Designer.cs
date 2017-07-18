d61511e803adadded7c95e62150c880be05f36a9
nikfluks <flukscro@gmail.com>
2017-07-18 22:13:54 +0200

    Omogućeno dodavanje ishodišta i odredišta
    Na glavnoj formi je omogućeno dodavanje kapaciteta ishodišta i potrebe odredišta. Također može se unijeti stvarni trošak prijevoza na nekoj relaciji.
    Logika nije napravljena!

--- Transportni_problem/Transportni_problem/FrmGlavna.Designer.cs
+++ Transportni_problem/Transportni_problem/FrmGlavna.Designer.cs
@@ -28,20 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBrojIshodista = new System.Windows.Forms.TextBox();
+            this.txtBrojOdredista = new System.Windows.Forms.TextBox();
+            this.btnKreirajPraznuTablicu = new System.Windows.Forms.Button();
+            this.pnlTablica = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Broj ishodišta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Broj odredišta:";
+            // 
+            // txtBrojIshodista
+            // 
+            this.txtBrojIshodista.Location = new System.Drawing.Point(116, 22);
+            this.txtBrojIshodista.Name = "txtBrojIshodista";
+            this.txtBrojIshodista.Size = new System.Drawing.Size(53, 20);
+            this.txtBrojIshodista.TabIndex = 1;
+            // 
+            // txtBrojOdredista
+            // 
+            this.txtBrojOdredista.AcceptsReturn = true;
+            this.txtBrojOdredista.Location = new System.Drawing.Point(116, 71);
+            this.txtBrojOdredista.Name = "txtBrojOdredista";
+            this.txtBrojOdredista.Size = new System.Drawing.Size(53, 20);
+            this.txtBrojOdredista.TabIndex = 2;
+            // 
+            // btnKreirajPraznuTablicu
+            // 
+            this.btnKreirajPraznuTablicu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnKreirajPraznuTablicu.Location = new System.Drawing.Point(208, 20);
+            this.btnKreirajPraznuTablicu.Name = "btnKreirajPraznuTablicu";
+            this.btnKreirajPraznuTablicu.Size = new System.Drawing.Size(184, 69);
+            this.btnKreirajPraznuTablicu.TabIndex = 3;
+            this.btnKreirajPraznuTablicu.Text = "Kreiraj praznu tablicu";
+            this.btnKreirajPraznuTablicu.UseVisualStyleBackColor = true;
+            this.btnKreirajPraznuTablicu.Click += new System.EventHandler(this.btnKreirajPraznuTablicu_Click);
+            // 
+            // pnlTablica
+            // 
+            this.pnlTablica.Location = new System.Drawing.Point(16, 125);
+            this.pnlTablica.Name = "pnlTablica";
+            this.pnlTablica.Size = new System.Drawing.Size(376, 261);
+            this.pnlTablica.TabIndex = 11;
             // 
             // FrmGlavna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 369);
+            this.ClientSize = new System.Drawing.Size(450, 423);
+            this.Controls.Add(this.pnlTablica);
+            this.Controls.Add(this.btnKreirajPraznuTablicu);
+            this.Controls.Add(this.txtBrojOdredista);
+            this.Controls.Add(this.txtBrojIshodista);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FrmGlavna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Transportni problem";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBrojIshodista;
+        private System.Windows.Forms.TextBox txtBrojOdredista;
+        private System.Windows.Forms.Button btnKreirajPraznuTablicu;
+        private System.Windows.Forms.Panel pnlTablica;
     }
 }
 
--- Transportni_problem/Transportni_problem/FrmGlavna.cs
+++ Transportni_problem/Transportni_problem/FrmGlavna.cs
@@ -16,5 +16,126 @@ namespace Transportni_problem
         {
             InitializeComponent();
         }
+
+        int brojIshodista;
+        int brojOdredista;
+
+        private void btnKreirajPraznuTablicu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                brojIshodista = int.Parse(txtBrojIshodista.Text);
+                brojOdredista = int.Parse(txtBrojOdredista.Text);
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Pogrešan unos!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            pnlTablica.Controls.Clear();
+
+            NacrtajRetke();
+            NacrtajStupce();
+            NacrtajCelije();
+
+        }
+
+        private void NacrtajRetke()
+        {
+            int y = 55;
+
+            for (int i = 1; i <= brojIshodista; i++)
+            {
+                Label labelaI = new Label();
+                labelaI.Text = "I" + i;
+                labelaI.Location = new Point(10, y);
+                labelaI.Size = new Size(30, 15);
+                //labelaI.Font = new Font(labelaI.Font.FontFamily, 15, FontStyle.Bold);
+                y += 45;
+
+                pnlTablica.Controls.Add(labelaI);
+            }
+
+            Label labelaIs = new Label();
+            labelaIs.Text = "Bj";
+            labelaIs.Location = new Point(10, y + 10);
+            labelaIs.Size = new Size(30, 15);
+
+            if (pnlTablica.Height < y)//ako je panel premali, tj sljedece ishodiste se nebi vidjelo, povecamo ga
+            {
+                pnlTablica.Height = y + 45;
+            }
+
+            pnlTablica.Controls.Add(labelaIs);
+        }
+
+        private void NacrtajStupce()
+        {
+            int x = 60;
+
+            for (int i = 1; i <= brojOdredista; i++)
+            {
+                Label labelaO = new Label();
+                labelaO.Text = "O" + i;
+                labelaO.Location = new Point(x, 10);
+                labelaO.Size = new Size(30, 15);
+                x += 65;
+
+                pnlTablica.Controls.Add(labelaO);
+            }
+
+            if (pnlTablica.Width < x)//ako je panel premali, tj sljedece odrediste se nebi vidjelo, povecamo ga
+            {
+                pnlTablica.Width = x + 65;
+            }
+
+            Label labelaOd = new Label();
+            labelaOd.Text = "Ai";
+            labelaOd.Location = new Point(x + 10, 10);
+            labelaOd.Size = new Size(30, 15);
+
+            pnlTablica.Controls.Add(labelaOd);
+        }
+
+        private void NacrtajCelije()
+        {
+            int x = 40;
+            int y = 40;
+            bool zadnji = false;
+
+            for (int i = 1; i <= (brojIshodista + 1) * (brojOdredista + 1); i++)
+            {
+                if (i % (brojOdredista + 1) == 0) 
+                {
+                    x += 10;
+                }
+
+                if (i > ((brojIshodista + 1) * (brojOdredista + 1)) - (brojOdredista + 1) && !zadnji)
+                {
+                    y += 10;
+                    zadnji = true;
+                }
+
+                TextBox celija = new TextBox();
+                celija.Location = new Point(x, y);
+                celija.Size = new Size(60, 40);
+                celija.Multiline = true;
+
+                if (i % (brojOdredista + 1) == 0) //ako smo dosli do kraja reda, prebacujemo se u novi red
+                {
+                    x = 40;
+                    y += 45;
+                }
+                else //inace se samo pomicemo udesno
+                {
+                    x += 65;
+                }
+
+                pnlTablica.Controls.Add(celija);
+            }
+
+        }
     }
 }
